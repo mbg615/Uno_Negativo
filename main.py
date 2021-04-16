81e5dc278651e0f0bcfaf260a0736deb3230c528
SirStahlmann <82391894+SirStahlmann@users.noreply.github.com>
2021-04-15 20:28:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-from pyglet.window import *
+from pyglet.window import Window, key
 from pyglet import app
 
-file = open("uno.config", "r")
+width = 800
+height = 600
+fus = 1
+window = Window(width, height, resizable = True)
 
-fullscreen = False
+#file = open("uno.config", "r")
 
-if fullscreen:
-    win = Window(fullscreen=True)
-else:
-    win = Window(width=800,height=600)
+def on_key_press(symbol, modifiers):
+    if symbol == key.F:
+        global fus
+        fus += 1
+
+def on_key_release(symbol, modifiers):
+    if symbol == key.F and fus % 2 == 0:
+        window.set_fullscreen(True)
+
+    if symbol == key.F and fus % 2 == 1:
+        window.set_fullscreen(False)
+
+def on_draw():
+    window.clear()
+
+window.push_handlers(on_key_press, on_key_release, on_draw)
+        
 app.run()
