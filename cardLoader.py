b993302b6c2237370acae49d1dea7e58f4696162
mgb615 <m4dpup@gmail.com>
2021-04-27 11:06:47 -0500

    add card draw functions

--- cardLoader.py
+++ cardLoader.py
@@ -1,4 +1,26 @@
-def makeDeck(decksUsed):
-    cardPool = []
+# Makes one standard Uno Deck from the cards.txt file
+def makeDeck():
     cardFile = open("data/cards.txt","r")
-    print(cardPool)
+    rawRead = cardFile.readlines()
+
+    # Strip newline (\n)
+    cardPool = []
+    for element in rawRead:
+        cardPool.append(element.strip())
+    return cardPool
+
+# Draw a card from a finite deck; The deck will refill its self if empty.
+def drawCardFinite(deck):
+    from random import randrange
+    randNum = randrange(len(deck))
+    card = deck[randNum]
+    deck = deck.pop(randNum)
+    if len(deck) == 0:
+        deck = makeDeck()
+    return card, deck
+
+def drawCardInfinite(deck):
+    from random import randrange
+    randNum = randrange(len(deck))
+    card = deck[randNum]
+    return card
